6ce5d3b2da1509acf232567d4df814b273c5defb
Aisulu <aidarkyzyaisulu@gmail.com>
2023-05-07 15:25:38 +0600

    Add service side

---
+++ src/app/services/dental.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { IService } from "../shared/service";
+import { IDentist } from "../shared/dentist";
+
+@Injectable({
+  providedIn: "root",
+})
+export class DentalService {
+  constructor(private http: HttpClient) {}
+
+  url: string = "https://dental-realtime-database-default-rtdb.firebaseio.com";
+
+  getServices() {
+    return this.http.get<IService[]>(`${this.url}/services.json`);
+  }
+
+  getDentists() {
+    return this.http.get<IDentist[]>(`${this.url}/dentists.json`);
+  }
+}
