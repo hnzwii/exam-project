d2f57c78cff9dca4945a3e28d5a717fa14c58497
Aisulu <aidarkyzyaisulu@gmail.com>
2023-05-07 15:43:24 +0600

    Connect components to the main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,36 @@ import { AppComponent } from "./app.component";
 import { MainComponent } from "./components/main/main.component";
 import { NavigationComponent } from "./components/navigation/navigation.component";
 import { FormsModule } from "@angular/forms";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { ServicesGroupComponent } from "./components/services-group/services-group.component";
+import { ServiceCardComponent } from "./components/service-card/service-card.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CarouselModule } from "ngx-owl-carousel-o";
+import { CardsCarouselComponent } from "./components/cards-carousel/cards-carousel.component";
+import { DentistCardComponent } from "./components/dentist-card/dentist-card.component";
+import { HttpClientModule } from "@angular/common/http";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @NgModule({
-  declarations: [AppComponent, MainComponent, NavigationComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    MainComponent,
+    NavigationComponent,
+    ServicesGroupComponent,
+    ServiceCardComponent,
+    CardsCarouselComponent,
+    DentistCardComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    MatSlideToggleModule,
+    BrowserAnimationsModule,
+    CarouselModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
