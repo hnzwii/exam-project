ae72bce6c6fa9ada8ef6e8e99870d9f00d8ff108
Aisulu <aidarkyzyaisulu@gmail.com>
2023-05-11 21:10:36 +0600

    Add Dentist component and buttons with limit

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Database, onValue, ref } from "@angular/fire/database";
+import { DentalService } from "src/app/services/dental.service";
+import { IDentist } from "src/app/shared/dentist";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.scss"],
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  dentists!: IDentist[];
+  limit: number = 2;
+  show: boolean = true;
 
+  constructor(private database: Database) {}
+
+  ngOnInit(): void {
+    const starCountRef = ref(this.database, `dentists/`);
+    console.log(this.database, starCountRef);
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log(this.database, data);
+      this.dentists = data;
+      console.log(this.dentists);
+    });
+  }
+
+  showMore() {
+    this.limit = this.dentists.length;
+    this.show = false;
+  }
+
+  showLess() {
+    this.limit = 2;
+    this.show = true;
+  }
 }
